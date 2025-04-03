24e58c102b4f962f447a9e6b0b6c30612cdb8a12
Meredith <meredith.moore@drake.edu>
2025-04-03 13:12:34 -0500

    small update, demo

--- flaskapp.py
+++ flaskapp.py
@@ -25,7 +25,7 @@ def add_user():
         # For now, let's just print it to the console
         print("Name:", name, ":", "Favorite Genre:", genre)
         
-        flash('User added successfully!', 'success')  # 'success' is a category; makes a green banner at the top
+        flash('User added successfully! Huzzah!', 'success')  # 'success' is a category; makes a green banner at the top
         # Redirect to home page or another page upon successful submission
         return redirect(url_for('home'))
     else:
